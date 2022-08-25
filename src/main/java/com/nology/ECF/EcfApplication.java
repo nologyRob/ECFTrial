0c88bc1c7a46289ad31dff323b873ef75a4dbd0d
nologyRob <rob.hooper@nology.io>
2022-08-25 12:14:14 +0100

    Initial Commit
    Signed-off-by: nologyRob <rob.hooper@nology.io>

--- src/main/java/com/nology/ECF/EcfApplication.java
+++ src/main/java/com/nology/ECF/EcfApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Objects;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EcfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcfApplication.class, args);
+
 	}
 
 }
